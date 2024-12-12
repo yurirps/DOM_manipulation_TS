a47342e9d837e28e278bad0963266e69cf540da2
Yuri Santos <yuri.rps@gmail.com>
2024-12-12 20:43:30 -0300

    Slide show

--- dist/Slide.js
+++ dist/Slide.js
@@ -1,17 +1,49 @@
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
-        console.log(this.container);
-        console.log(this.elements);
-        console.log(this.controls);
-        console.log(this.time);
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.init();
+    }
+    hide(el) {
+        el.classList.remove('active');
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((el) => this.hide(el));
+        this.slide.classList.add('active');
+    }
+    prev() {
+        const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    next() {
+        const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    addControls() {
+        const prevButton = document.createElement('button');
+        const nextButton = document.createElement('button');
+        prevButton.innerText = "Slide Anterior";
+        nextButton.innerText = "Próximo Slide";
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        prevButton.addEventListener('pointerup', () => this.prev());
+        nextButton.addEventListener('pointerup', () => this.next());
+    }
+    init() {
+        this.addControls();
+        this.show(this.index);
     }
 }
 //# sourceMappingURL=Slide.js.map
